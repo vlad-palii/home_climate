c93cbef752d42276cef82eebcf51fb8a8c914eef
Vlad Palii <paliy.work@gmail.com>
2019-03-03 16:20:57 +0200

    cleanup

--- home_dummy/home_dummy_app/templatetags/get_temp_once.py
+++ home_dummy/home_dummy_app/templatetags/get_temp_once.py
@@ -8,19 +8,13 @@ ser.flushInput()
 
 @register.simple_tag
 def get_temp_once():
-    print("i'm alive!")
     while True:
         result = ""
         try:
             ser.read_until('Start', 124)
             result = ser.readline()
             if result != "":
-                print(result.decode().strip())
                 return result.decode().strip()
         finally:
             if result != "":
-                print(result.decode().strip())
                 return result.decode().strip()
-
-
-get_temp_once()
--- home_dummy/home_dummy_app/views.py
+++ home_dummy/home_dummy_app/views.py
@@ -1,7 +1,4 @@
-from django.http import HttpResponse
 from django.shortcuts import render
-#from home_dummy_app.templatetags.get_temp_once import get_temp_once
 
 def index(request):
-    #get_temp_once()
     return render(request, "index.html")
