232e8dec82a7f1e94c8a0f669a35d76b306b32c1
Vlad <paliy.work@gmail.com>
2019-08-23 17:49:44 +0300

    Add some acid header color.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,7 +9,7 @@ export default function App() {
   return (
     <div id="app" className="synthwave-i">
     <Router>
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <nav className="navbar navbar-expand-lg navbar-light bg-header-synthwave">
         <a className="navbar-brand" href="#"></a>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
